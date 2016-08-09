e55f5e61ea276868e2c8803fcec0c5400d62a39a
pawel franczak <pawel.fran@gmail.com>
2016-08-09 20:54:04 +0200

    initial commit

---
+++ film.py
@@ -0,0 +1,22 @@
+import re
+
+class Film:
+    def __init__(self, title, polish_title, year, link, country, genre):
+        self.polish_title = polish_title
+        self.year = year
+        self.link = link
+        self.title = title
+        self.country = country
+        self.genre = genre
+
+    def __str__(self):
+        s = "title: " + self.title
+        s += ", polish_title: " + self.polish_title
+        s += ", year: " + str(self.year)
+        s += ", link: " + self.link
+        s += ", country: " + self.country
+        s += ", genre: " + self.genre
+
+        return s
+
+
